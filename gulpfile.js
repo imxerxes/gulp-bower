7157e88d04c1bea8a3948aa127ca88f4240a343a
Kent McNeill <imxerxes@gmail.com>
2014-08-21 22:16:37 +1000

    init

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var plugins = require('gulp-load-plugins')();
+
+gulp.task('bower', function() {
+  return plugins.bower()
+    .pipe(gulp.dest('lib/'))
+});
+
+gulp.task('webserver', function() {
+  gulp.src('app')
+    .pipe(plugins.webserver({
+      livereload: true,
+      directoryListing: true,
+      open: true
+    }));
+});
+
+gulp.task('wiredep', function () {
+  var wiredep = require('wiredep').stream;
+
+  gulp.src('app/styles/*.css')
+    .pipe(wiredep({
+        directory: 'app/bower_components'
+    }))
+    .pipe(gulp.dest('app/styles'));
+
+  gulp.src('app/d3.html')
+    .pipe(wiredep({
+      directory: 'bower_components'
+    }))
+    .pipe(gulp.dest('app'));
+});
+
+gulp.task('default', ['bower', 'wiredep', 'webserver']);
